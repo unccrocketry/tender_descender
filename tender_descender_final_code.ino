3134ba517dd6dcab84597d9d5c4c2b4f1e4c0485
unccrocketry <45529186+unccrocketry@users.noreply.github.com>
2019-02-09 23:22:40 -0500

    Calibrated Changes for First Launch Test

--- tender_descender_final_code.ino
+++ tender_descender_final_code.ino
@@ -12,14 +12,26 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
 #include "DFRobot_BMP280.h"
+
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
 DFRobot_BMP280 bmp;
 bool tareBit = false;
 bool takeoff = false;
 bool apogee = false;
+bool timerBool = false;
+
+int timerBoolVal = 60000; //[milliseconds] We want to make sure that at least 60 seconds have passed before we can fire the ematches.
+
+
 int startHeight = 0;
 volatile int maxHeight = 0;
+
+
+unsigned long startTimer;
+unsigned long endTimer;
+
+
 
 int e1 = 23;
 int e2 = 22;
@@ -33,6 +45,8 @@ double zacc;
 
 void setup()
 {
+  maxHeight = 0;
+  
  /* Initialise the barometer */
   while (!bmp.begin()) {  
     delay(1000);
@@ -80,33 +94,56 @@ void loop()
   if ((acc_mag > 20)&&(takeoff==false))
   {
     takeoff = true;
+    startTimer = millis();
   }
 
   if ((takeoff == true) && (apogee==false))
   {
     /* Compare current height to the maximum height & update if necessary. */
+    alt = int((bmp.readAltitudeValue()-startHeight)*3.28084);
     if (alt > maxHeight+3)
     {
       maxHeight = alt;
     }    
-
+    
+  delay(50); //Delay to decrease chances of capturing a spike
+    
     /* If current height is less than 10 feet of the maximum recorded height, the rocket has reached apogee and is descending. */
-    if (alt < maxHeight-10)
+    if ((alt < maxHeight-10) && (apogee == false))
+    
     {
-      apogee=true;
+      delay(100); //wait one hundred milliseconds and check again to make sure we didn't capture a spike.
+      alt = int((bmp.readAltitudeValue()-startHeight)*3.28084);
+      if (alt < maxHeight - 10) //
+      {
+        apogee=true;
+      }
     }
   
   }
 
   if (apogee==true)
   {
-    /* As the rocket is descending, if an altitude of less than 20ft is observed, and the magnitude acceleration is greater than 20
+    /* As the rocket is descending, if an altitude of less than 20ft is observed, and the magnitude acceleration is greater than 20 and specified time out has happened.
     the two e-matches and the led pins on-board the Teensy 3.2 are set to HIGH. */
-    if ((alt < 20) && (acc_mag > 20))
-    {
-      digitalWrite(led,HIGH);
-      digitalWrite(e1,HIGH);
-      digitalWrite(e2,HIGH);
+    if (timerBool == true)
+   {
+      alt = int((bmp.readAltitudeValue()-startHeight)*3.28084);
+      if ((alt < 20) && (acc_mag > 20))
+      {
+        digitalWrite(led,HIGH);
+        digitalWrite(e1,HIGH);
+        digitalWrite(e2,HIGH);
+      }
     }
+  }
+
+  if (takeoff == true)
+  {
+    endTimer = millis();   
+    if ((endTimer - startTimer) > timerBoolVal)
+    {
+       timerBool = true;
+       }
   }
   }
